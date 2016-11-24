795f0ba2d6df01e54dfc53b4e7fa619cf6e892e2
Otávio Fernandes <ofernandes@schubergphilis.com>
2016-11-24 06:24:39 +0100

    Less.

--- of-speedbar/packages.el
+++ of-speedbar/packages.el
@@ -3,21 +3,7 @@
 ;;  File: packages.el
 ;;
 
-(defconst of-speedbar-packages '(sr-speedbar
-                                 projectile-speedbar
-                                 ppd-sr-speedbar))
-
-(defun of-speedbar/post-init-ppd-sr-speedbar ()
-  (use-package ppd-sr-speedbar
-    :ensure t
-    )
-  )
-
-(defun of-speedbar/post-init-projectile-speedbar ()
-  (use-package projectile-speedbar
-    :ensure t
-    )
-  )
+(defconst of-speedbar-packages '(sr-speedbar))
 
 (defun of-speedbar/init-sr-speedbar ()
   (use-package sr-speedbar
@@ -34,55 +20,54 @@
      sr-speedbar-verbosity-level 0
      sr-speedbar-width-console 23
      sr-speedbar-width-x 32
+     sr-speedbar-skip-other-window-p t
      ;; adding tags to support more extensions on sr-speedbar
-     speedbar-supported-extension-expressions
-     '(".[ch]\\(\\+\\+\\|pp\\|c\\|h\\|xx\\)?"
-       ".tex\\(i\\(nfo\\)?\\)?"
-       ".todo"
-       ".done"
-       ".md"
-       ".el"
-       ".emacs"
-       ".l"
-       ".lsp"
-       ".p"
-       ".java"
-       ".js"
-       ".f\\(90\\|77\\|or\\)?"
-       ".ad[abs]"
-       ".p[lm]"
-       ".tcl"
-       ".m"
-       ".scm"
-       ".pm"
-       ".py"
-       ".g"
-       ".go"
-       ".rb"
-       ".jpg"
-       ".png"
-       "\\.\\(inc\\|php[s345]?\\|phtml\\)"
-       ".s?html"
-       ".ma?k"
-       "[Mm]akefile\\(\\.in\\)?")
-      )
+     speedbar-supported-extension-expressions '(".[ch]\\(\\+\\+\\|pp\\|c\\|h\\|xx\\)?"
+                                                ".tex\\(i\\(nfo\\)?\\)?"
+                                                ".todo"
+                                                ".done"
+                                                ".md"
+                                                ".el"
+                                                ".emacs"
+                                                ".l"
+                                                ".lsp"
+                                                ".p"
+                                                ".java"
+                                                ".js"
+                                                ".f\\(90\\|77\\|or\\)?"
+                                                ".ad[abs]"
+                                                ".p[lm]"
+                                                ".tcl"
+                                                ".m"
+                                                ".scm"
+                                                ".pm"
+                                                ".py"
+                                                ".g"
+                                                ".go"
+                                                ".rb"
+                                                ".jpg"
+                                                ".png"
+                                                "\\.\\(inc\\|php[s345]?\\|phtml\\)"
+                                                ".s?html"
+                                                ".ma?k"
+                                                "[Mm]akefile\\(\\.in\\)?"))
     (define-key speedbar-mode-map
       (kbd "q") 'spacemacs/sr-speedbar-show-or-hide)
     )
   )
 
 (defun of-speedbar/post-init-sr-speedbar ()
-  (defun spacemacs/sr-speedbar-show-or-hide ()
-    (interactive)
-    (cond ((sr-speedbar-exist-p)
-           (kill-buffer speedbar-buffer))
-          (t
-           (sr-speedbar-open)
-           (linum-mode -1)
-           (speedbar-refresh)
-           )
-          )
-    )
+;;(defun spacemacs/sr-speedbar-show-or-hide ()
+;;  (interactive)
+;;  (cond ((sr-speedbar-exist-p)
+;;         (kill-buffer speedbar-buffer))
+;;        (t
+;;         (sr-speedbar-open)
+;;         (linum-mode -1)
+;;         (speedbar-refresh)
+;;         )
+;;        )
+;;  )
 
   (add-hook 'speedbar-mode-hook (lambda() (set-window-fringes nil 0)))
   (add-hook 'go-mode-hook (lambda () (speedbar-add-supported-extension ".go")))
